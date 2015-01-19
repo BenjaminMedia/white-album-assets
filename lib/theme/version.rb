0143aadfadde2d9ede0dc8c9c20e651b19918331
Johan Frølich <johanfrolich@gmail.com>
2015-01-19 15:19:20 +0100

    Removed dev

--- lib/theme/version.rb
+++ lib/theme/version.rb
@@ -3,13 +3,12 @@ module Theme
     @major = 1
     @minor = 1
     @tiny  = 0
-    @build = 'dev'
 
     class << self
-      attr_reader :major, :minor, :tiny, :build
+      attr_reader :major, :minor, :tiny
 
       def to_s
-        [@major, @minor, @tiny, @build].compact.join('.')
+        [@major, @minor, @tiny].compact.join('.')
       end
     end
   end
